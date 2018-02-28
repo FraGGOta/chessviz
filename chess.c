f0077be29a28ae7983981d9f393da9c6a80a0214
Рамус Евгений Сергеевич <is741s19@awp09.csc.local>
2018-02-28 14:05:48 +0700

    Fixed chessviz

--- chess.c
+++ chess.c
@@ -2,15 +2,17 @@
 
 int main()
 {
-    printf("|r|k|b|k|0|b|k|r|\n");
-    printf("|p|p|p|p|p|p|p|p|\n");
+    printf(" |a|b|c|d|e|f|g|h|\n");
+    printf("1|r|k|b|k|q|b|k|r|\n");
+    printf("2|p|p|p|p|p|p|p|p|\n");
     
     for (int i = 0; i < 4; ++i) { 
-	printf("| | | | | | | | |\n");
+	int r = i + 3;
+	printf("%d| | | | | | | | |\n", r);
     }
     
-    printf("|p|p|p|p|p|p|p|p|\n");
-    printf("|r|k|b|k|0|b|k|r|\n");
+    printf("7|P|P|P|P|P|P|P|P|\n");
+    printf("8|R|K|B|K|Q|B|K|R|\n");
     return 0;
 }
     
