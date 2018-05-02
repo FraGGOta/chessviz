9a84fe4b3dae7847813523d167736760d5d23143
Евгений <eugene@localhost.localdomain>
2018-05-02 21:10:53 +0700

    Fixed board.h

--- src/board.h
+++ src/board.h
@@ -5,12 +5,24 @@
 #include <stdlib.h>
 
 typedef struct {
-    char array[8];
     char type;
+    int colour;
 }board;
 
-int coordinates(board *a);
+void initial_board(board *a);
 
-void swap(board *a, char i, int j, char i1, int j1);
+void print_board(board *a);
+
+int coordinates(char i, int j, char i1, int j1);
+
+int *convert(char j, int i, char j1, int i1);
+
+void move(board *a, char i, int j, char i1, int j1);
+
+int colour_checking(board * a, int str, int stl, int colour);
+
+int win_checking(board * a);
+
+int move_checking(board * a, int i, int j, int i1, int j1);
 
 #endif
